b8e99b26c4ef97f113bd673d03e661061e22b000
noelr <noelr@SANDINO01>
2021-10-04 18:36:20 +0800

    Add project files.

---
+++ Asset.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asset.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
